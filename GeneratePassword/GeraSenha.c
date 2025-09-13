ca71dc03d92601207bb3a73df32d183c31dabd2e
LdevM <luismachadoneto12@gmail.com>
2025-09-13 01:58:02 +0000

    Second commit with new features and bugs fixed

--- GeneratePassword/GeraSenha.c
+++ GeneratePassword/GeraSenha.c
@@ -4,8 +4,9 @@
 #include <stdbool.h>
 #include <windows.h>
 #include <ctype.h>
-#include <strings.h>
-
+#include <string.h>
+#include <locale.h>
+#include <errno.h>
 
 void removeExceptions(const int length, char *password) {
     for (int i = 0; i < length; i++) {
@@ -71,36 +72,37 @@ bool VerifyExtensionExisting(const char *nameArchive){
 
 
 void SaveNewArchive(char *password, char *nameArchive) {
-    FILE *GuardaSenha;
+    FILE *savePassword;
     if (!VerifyExtensionExisting(nameArchive)){
         addFinalTxt(nameArchive);
     }
 
-    GuardaSenha = fopen(nameArchive, "w");
-    if (GuardaSenha == NULL) {
-        printf("Erro ao abrir arquivo!\n");
+    savePassword = fopen(nameArchive, "w");
+    if (savePassword == NULL) {
+        perror("Erro ao abrir arquivo!");
         return;
     }
 
-    fprintf(GuardaSenha, "%s\n", password);
-    fclose(GuardaSenha);
+    fprintf(savePassword, "%s\n", password);
+    fclose(savePassword);
 }
 
 
 
 void SaveExistingArchive(char *password, char *nameArchive) {
-    FILE *GuardaSenha;
-    if (!VerifyExtensionExisting(nameArchive));{
+    FILE *savePassword;
+    if (!VerifyExtensionExisting(nameArchive)){
         addFinalTxt(nameArchive);
     }
 
-    GuardaSenha = fopen(nameArchive, "a");
-    if (GuardaSenha == NULL) {
-        printf("Erro ao abrir arquvio!\n");
+    savePassword = fopen(nameArchive, "a");
+
+    if (savePassword == NULL) {
+        perror("Erro ao abrir arquvio!");
         return;
     }
-    fprintf(GuardaSenha, "%s\n", password);
-    fclose(GuardaSenha);
+    fprintf(savePassword, "%s\n", password);
+    fclose(savePassword);
 
 }
 
@@ -109,6 +111,7 @@ void SaveExistingArchive(char *password, char *nameArchive) {
 bool IsExtensionTxt(const char *nameArchive) {
     const char Txt[] = ".txt";
     char *extensao_ptr;
+    bool extensionTxt = false;
 
 
     extensao_ptr = strrchr(nameArchive, '.');
@@ -118,7 +121,41 @@ bool IsExtensionTxt(const char *nameArchive) {
         return true;
     }
 
-    return false;
+    return extensionTxt;
+}
+
+void ReadArchive(char *nameArchive) {
+    FILE *savePassword;
+    char bufferLine[30];
+
+    savePassword = fopen(nameArchive, "r");
+
+    if (savePassword == NULL){
+        perror("Erro ao abrir arquivo");
+        return;
+    }
+
+    while(fgets(bufferLine, 30, savePassword) != NULL){
+        printf("%s", bufferLine);
+    }
+
+    fclose(savePassword);
+}
+
+void renameArchive(const char *nameArchive, char *newName){
+    int aux = 0;
+    if (!VerifyExtensionExisting(newName)){
+        addFinalTxt(newName);
+    }
+
+    aux = rename(nameArchive, newName);
+
+    if (aux == 0){
+        printf("Nome alterado de: %s para %s!\n", nameArchive, newName);
+    }
+    else{
+        perror("Erro ao renomear arquivo!\n");
+    }
 }
 
 int main() {
@@ -127,112 +164,192 @@ int main() {
     char *password = NULL;
     char nameArchive[50];
     char answerYN;
+    char newNameArchive[50];
+    bool keepRunningProgram = false;
+    setlocale(LC_ALL, "");
+    do{
+        do {
+            printf("Tamanho da senha (Mï¿½x: 20):");
+            if (scanf("%d", &size) == 1 && (size > 0) && (size <= 20))
+                break;
+            else {
+                printf("Dado invï¿½lido!\nTente novamente\n");
+                clearInputBuffer();
+            }
+        } while (true);
 
-    do {
-        printf("Tamanho da senha (Máx: 20):");
-        if (scanf("%d", &size) == 1 && (size > 0) && (size <= 20))
-            break;
-        else {
-            printf("Dado inválido!\nTente novamente\n");
-            clearInputBuffer();
+        password = (char*)malloc((size + 1) * sizeof(char));
+
+        if (password == NULL) {
+            printf("Erro ao alocar memï¿½ria\n");
+            return 1;
         }
-    } while (true);
 
-    password = (char*)malloc((size + 1) * sizeof(char));
+        do {
+            printf("Escolha a dificulade da senha\n\n");
+            printf("(1)Fï¿½cil\n");
+            printf("(2)Mï¿½dio\n");
+            printf("(3)Difï¿½cil\n");
+            printf("Sua opï¿½ï¿½o: ");
+            if (scanf("%d", &choice) == 1 && (choice >= 1 && choice <= 3))
+                break;
+            else {
+                printf("Resposta invï¿½lida!\nTente novamente\n");
+                Sleep(3000);
+                system("cls");
+                clearInputBuffer();
+            }
+        } while (true);
 
-    if (password == NULL) {
-        printf("Erro ao alocar memória\n");
-        return 1;
-    }
+        generatePassword(size, choice, password);
+        printf("%s\n", password);
 
-    do {
-        printf("Escolha a dificulade da senha\n\n");
-        printf("(1)Fácil\n");
-        printf("(2)Médio\n");
-        printf("(3)Difícil\n");
-        if (scanf("%d", &choice) == 1 && (choice >= 1 && choice <= 3))
-            break;
-        else {
-            printf("Dado inválido!\nTente novamente\n");
-            Sleep(3000);
-            system("cls");
-            clearInputBuffer();
-        }
-    } while (true);
-
-    generatePassword(size, choice, password);
-    printf("%s\n", password);
-
-    clearInputBuffer();
-
-    do {
-        printf("Você gostaria de salvar a senha em um arquivo de texto? [S/N]");
-        scanf("%c", &answerYN);
         clearInputBuffer();
-        answerYN = toupper(answerYN);
 
-        if (answerYN == 'S' || answerYN == 'N') {
-            if (answerYN == 'N')
-                return 0;
-            break;
-        } else {
-            printf("Resposta inválida! Tente novamente\n");
+        do {
+            printf("Vocï¿½ gostaria de salvar a senha em um arquivo de texto? [S/N]\n");
+            printf("Sua opï¿½ï¿½o: ");
+            scanf("%c", &answerYN);
+            clearInputBuffer();
+            answerYN = toupper(answerYN);
+
+            if (answerYN == 'S' || answerYN == 'N') {
+                if (answerYN == 'N'){
+                    free(password);
+                    return 0;
+                }
+                break;
+            } else {
+                printf("Resposta invï¿½lida! Tente novamente\n");
+            }
+        } while (true);
+
+        do {
+            printf("Selecione uma opï¿½ï¿½o:\n");
+            printf("[1] Guardar em um novo arquivo\n");
+            printf("[2] Adicionar em um arquivo existente\n");
+            printf("Sua opï¿½ï¿½o: ");
+            if (scanf("%d", &choice) == 1 && (choice >= 1 && choice <= 2)){
+                break;
+            }
+            else {
+                printf("Resposta invï¿½lida! Tente novamente\n");
+                Sleep(3000);
+                system("cls");
+                clearInputBuffer();
+            }
+        } while (true);
+
+        if (choice == 1){
+            do{
+                printf("Como deseja salvar o nome do arquivo?\n");
+                scanf("%49s", nameArchive);
+                if(VerifyExtensionExisting(nameArchive) && !IsExtensionTxt(nameArchive)){
+                    printf("Extensï¿½o nï¿½o permitida, use .txt ou salve sem extensï¿½o!\n");
+                    Sleep(3000);
+                    system("cls");
+                    clearInputBuffer();
+                }
+                else{
+                    SaveNewArchive(password, nameArchive);
+                    break;
+                }
+            } while(true);
+
+
         }
-    } while (true);
+        if (choice == 2) {
+            do{
+                printf("Digite o nome do arquivo existente (O nome deve ser exato, caso contrï¿½rio serï¿½ criado um novo arquivo): ");
+                scanf("%s", nameArchive);
+                if (VerifyExtensionExisting(nameArchive) && !IsExtensionTxt(nameArchive)){
+                    printf("Extensï¿½o nï¿½o permitida, use .txt ou salve sem extensï¿½o!\n");
+                    Sleep(3000);
+                    system("cls");
+                    clearInputBuffer();
+                }
+                else{
+                    SaveExistingArchive(password, nameArchive);
+                    break;
+                }
+            } while(true);
+        }
 
-    do {
-        printf("Selecione uma opção:\n");
-        printf("[1] Guardar em um novo arquivo\n");
-        printf("[2] Adicionar em um arquivo existente\n");
-        if (scanf("%d", &choice) == 1 && (choice >= 1 && choice <= 2))
-            break;
-        else {
-            printf("Resposta inválida! Tente novamente\n");
-            Sleep(3000);
+        do{
+            printf("Selecione uma opï¿½ï¿½o:\n");
+            printf("[1]Ler as senhas deste arquivo\n");
+            printf("[2]Renomear o arquivo\n");
+            printf("[3]Encerrar o programa\n");
+            printf("[4]Gerar uma nova senha\n");
+            printf("Sua opï¿½ï¿½o:");
+            if (scanf("%d", &choice) == 1 && (choice > 0 && choice <= 4)){
+                break;
+            }
+            else{
+                printf("Resposta invï¿½lida!\nTente novamente\n");
+                Sleep(3000);
+                system("cls");
+                clearInputBuffer();
+            }
+        } while(true);
+
+        if (choice == 1){
+            ReadArchive(nameArchive);
+        }
+
+        if (choice == 2){
+            do{
+                printf("Qual o nome que deseja renomear?\n");
+                scanf("%49s", newNameArchive);
+                if(VerifyExtensionExisting(newNameArchive) && !IsExtensionTxt(newNameArchive)){
+                    printf("Arquivo com extensï¿½o nï¿½o permitida! Tente novamente\n");
+                    Sleep(3000);
+                    system("cls");
+                    clearInputBuffer();
+                }
+                else{
+                    rename(nameArchive, newNameArchive);
+                    break;
+                }
+            } while(true);
+        }
+
+        if (choice == 3){
+            free(password);
+            return 0;
+        }
+        if (choice == 4){
+            keepRunningProgram = true;
+            sleep(3000);
             system("cls");
             clearInputBuffer();
         }
-    } while (true);
-
-    if (choice == 1){
         do{
-            printf("Como deseja salvar o nome do arquivo?\n");
-            scanf("%49s", nameArchive);
-            if(VerifyExtensionExisting(nameArchive) && !IsExtensionTxt(nameArchive)){
-                printf("Extensão não permitida, use .txt ou salve sem extensão!\n");
+            if(choice == 4)
+                break;
+            printf("Selecione uma opï¿½ï¿½o:\n");
+            printf("[1] Gerar uma nova senha\n");
+            printf("[2] Encerrar o programa\n");
+            printf("Sua opï¿½ï¿½o:");
+            if (scanf("%d", &choice) == 1 && (choice > 0 && choice <= 2)){
+                break;
+            }
+            else{
+                printf("Resposta invï¿½lida!\nTente novamente\n");
                 Sleep(3000);
                 system("cls");
                 clearInputBuffer();
             }
-            else{
-                SaveNewArchive(password, nameArchive);
-                break;
-            }
         } while(true);
 
-
-    }
-    if (choice == 2) {
-        do{
-            printf("Digite o nome do arquivo existente (O nome deve ser exato, caso contrário será criado um novo arquivo):");
-            scanf("%s", nameArchive);
-            if (VerifyExtensionExisting(nameArchive) && !IsExtensionTxt(nameArchive)){
-                printf("Extensão não permitida, use .txt ou salve sem extensão!\n");
-                Sleep(3000);
-                system("cls");
-                clearInputBuffer();
-
-            }
-            else{
-                SaveExistingArchive(password, nameArchive);
-                break;
-            }
-        } while(true);
-    }
-
-
-
-    free(password);
-
+        if (choice == 1)
+            keepRunningProgram = true;
+        if (choice == 2)
+            keepRunningProgram = false;
+        free(password);
+        Sleep(3000);
+        system("cls");
+        clearInputBuffer();
+    } while(keepRunningProgram);
     return 0;
 }
